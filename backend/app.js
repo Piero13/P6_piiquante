2feee3126714f5e32f2f13f5e6bd97ddcfe3e0fb
Pierre Fasce <lemonpiero@gmail.com>
2022-02-13 22:42:57 +0100

    installation et implémentation express-mongo-sanitize helmet dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require ('express');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+// const path = require('path');
+// const sauceRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
 const app = express();
-// const path = require('path')
+app.use(helmet());
 
-// const sauceRoutes = require('./routes/sauces')
-const userRoutes = require('./routes/user')
-
-mongoose.connect('mongodb+srv://Piero13:pScafe1981@pfdw13.egmg9.mongodb.net/pfdw13piiquante?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://' + process.env.MDB_user + ':' + process.env.MDB_pw + '@pfdw13.egmg9.mongodb.net/pfdw13piiquante?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -19,6 +23,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 // app.use('./images', express.static(path.join(__dirname, 'images')));
 // app.use('/api/sauces', sauceRoutes);
