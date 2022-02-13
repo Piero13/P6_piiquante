604dd8d3d763a35aa33fefdedebe25759f091d72
Pierre Fasce <lemonpiero@gmail.com>
2022-02-13 11:31:59 +0100

    installation des packages et création des fichiers server & app

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const express = require ('express');
+const app = express();
+const path = require('path')
+
+const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user')
+
+mongoose.connect('mongodb+srv://Piero13:pScafe1981@cluster0.egmg9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('./images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
