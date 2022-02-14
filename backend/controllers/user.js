dfefa38151be6a9ba95fe8f9d41d5f52b03adf2a
Pierre Fasce <lemonpiero@gmail.com>
2022-02-14 15:55:31 +0100

    installation node validator, correction du contrôleur utilisateur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,24 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 /* Fonction d'inscription */ 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
-                .catch(error => res.status(400).json({ error }));
-        })
+    if(validator.isEmail(req.body.email, {blacklisted_chars: '$="'})) {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                });
+                user.save()
+                    .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+    } else {
+        res.status(400).json({ error: "Le format de l'adresse email est invalide"});
+    }
 };
 
 /* Fonction de connexion */
@@ -21,7 +26,7 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if(!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé'});
+                return res.status(401).json({ error: 'Utilisateur inconnu'});
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
