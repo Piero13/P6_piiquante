fc3253705df8ad1da17e8152f4a163e73f728d30
Pierre Fasce <lemonpiero@gmail.com>
2022-02-15 19:02:02 +0100

    création du controleur pour la suppression d'une sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -78,7 +78,16 @@ exports.modifySauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée'}))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.likeSauce = (req, res, next) => {
